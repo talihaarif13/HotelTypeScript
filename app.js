eeb9c1725d785cb61ceecdd5eaa960e46ad02402
talihaarif13 <taliha.arif@invozone.com>
2021-12-27 13:51:49 +0500

    make a zip using path of uploaded multiple files and write that zip to disk

--- app.js
+++ app.js
@@ -29,6 +29,40 @@ app.use('/room', roomRoutes);
 app.use('/reservation', reservationRoutes);
 app.use('/services', serviceRoutes);
 
+
+//zip files
+const multer = require('multer');
+const path = require('path');
+const admzip = require('adm-zip');
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'uploads');
+    },
+
+    // By default, multer removes file extensions so let's add them back
+    filename: function(req, file, cb) {
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
+});
+
+let upload = multer({ storage: storage});
+
+app.post("/file/zip", upload.array("documents", 5), (req, res) => {
+    var zip = new admzip();
+    var outputFilePath = Date.now() + "output.zip";
+    if (req.files) {
+        req.files.forEach((file) => {
+            console.log(file.path)
+            zip.addLocalFile(file.path)
+        });
+        var willSendthis = zip.toBuffer();
+        // or write everything to disk
+        zip.writeZip(outputFilePath);
+        res.status(200).send("ok");
+    }
+});
+
 app.listen(3000, () => {
     console.log("app listening on 3000");
 })
